0dde7f41a9736445a9c9eca0406bedab07d70c16
Narendra Reddy <45414473+narendrareddy924@users.noreply.github.com>
2019-01-28 11:34:06 +0530

    deleted

--- myFiles/JS/27_JS_DOM_EventListeners/27_app.js
+++
@@ -1,7 +0,0 @@
-// Triangle One
-var textBoxOne = document.querySelector('#triangleOne');
-var displayTextOne = document.querySelector('#triangleOneText');
-textBoxOne.addEventListener('keyup',function() {
-    var textEntered = textBoxOne.value;
-    displayTextOne.textContent = triangleOne(textEntered);
-});
